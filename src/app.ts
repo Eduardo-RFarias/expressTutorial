c561cfd0293f2783ad90e6f53a7a63a12c7b8dcf
Eduardo-RFarias <eduardo.rfarias@outlook.com>
2022-01-30 14:16:54 -0300

    update tsconfig and npm to yarn config

--- src/app.ts
+++ src/app.ts
@@ -4,5 +4,6 @@ import server from "./server";
 const PORT = process.env.PORT || "3000";
 
 server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server is listening on port ${PORT}`);
 });
